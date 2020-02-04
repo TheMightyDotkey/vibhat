3879d681a2d7bc5973d7a1d7ee34c5c6e697359b
Nick <nickwilczewski@gmail.com>
2020-02-04 17:05:09 -0600

    streamline

--- examples/python/mcc118/vibhat.py
+++ examples/python/mcc118/vibhat.py
@@ -193,12 +193,12 @@ def read_and_display_data(hat, num_channels):
             for i in range(0, totalSamples, num_channels):
                 myArray.append([])  #add a row to the array (COLUMN)
                 for j in range(num_channels):
-                    print('{:10.5f}'.format(read_result.data[j]), 'V ',
+                    #print('{:10.5f}'.format(read_result.data[j]), 'V ',
                         end='')
                 	#append a num_channels of data to the array (ROW)
                     myArray[new_index].append(read_result.data[i + j])  
                 new_index+=1
-                print("\r")
+                #print("\r")
 
             csvwriter.writerows(myArray) #Write the array to file
             csvfile.flush
