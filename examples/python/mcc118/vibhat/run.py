3108b7baae414b899a8955f3bdb037362650de0d
Nick <nickwilczewski@gmail.com>
2020-02-04 19:00:01 -0600

    repeats every 60 sec

--- examples/python/mcc118/vibhat/run.py
+++ examples/python/mcc118/vibhat/run.py
@@ -4,19 +4,24 @@
 import vibhat
 import zipfile
 import os
+from time import sleep
 
 def main():
     """
     runs vibhat every minute, compresses vibhat data
 
     """
-    FDT = vibhat.vibhat()
-    print(FDT)
-    print('\n')
-    zipname = FDT[:-4]
-    filenameonly = FDT.split(os.sep)[-1]
-    zipfile.ZipFile(zipname, 'w', zipfile.ZIP_DEFLATED).write(FDT, filenameonly)
-    os.remove(FDT)
+    while True:
+
+        FDT = vibhat.vibhat()
+        print(FDT)
+        print('\n')
+        zipname = FDT[:-4]
+        filenameonly = FDT.split(os.sep)[-1]
+        zipfile.ZipFile(zipname, 'w', zipfile.ZIP_DEFLATED).write(FDT, filenameonly)
+        os.remove(FDT)
+        time.sleep(60)
+        
 
 if __name__ == '__main__':
     main()
