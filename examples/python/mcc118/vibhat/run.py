9e6dd02c8cc7734c82e2115d9e10ad9c84154875
Nick <nickwilczewski@gmail.com>
2020-04-27 00:41:15 -0500

    this is it

--- examples/python/mcc118/vibhat/run.py
+++ examples/python/mcc118/vibhat/run.py
@@ -21,9 +21,8 @@ def main():
         zipfile.ZipFile(zipname, 'w', zipfile.ZIP_DEFLATED).write(FDT, filenameonly)
         os.remove(FDT)
         sleep(5)
-        subprocess.check_call("rclone", "copy", "--config", "/home/pi/.rclone.conf /home/pi/New", "seniordesign:")
-        os.system('rclone copy /home/pi/New seniordesign:')
-        os.system('rclone copy /home/pi/Documents/Measurement_Computing/Scanning_log_files seniordesign:')
+        os.system('rclone --config="/home/pi/.config/rclone/rclone.conf" copy /home/pi/New seniordesign:')
+        os.system('rclone --config="/home/pi/.config/rclone/rclone.conf" copy /home/pi/Documents/Measurement_Computing/Scanning_log_files seniordesign:')
         sleep(60)
         
 
