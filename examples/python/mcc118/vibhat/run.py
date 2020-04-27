f1066fae6ba185dd8287f7631f78f8e38cf36528
Nick <nickwilczewski@gmail.com>
2020-04-26 23:42:57 -0500

    ok

--- examples/python/mcc118/vibhat/run.py
+++ examples/python/mcc118/vibhat/run.py
@@ -21,7 +21,7 @@ def main():
         zipfile.ZipFile(zipname, 'w', zipfile.ZIP_DEFLATED).write(FDT, filenameonly)
         os.remove(FDT)
         sleep(5)
-        os.system ('rclone copy /home/pi/New seniordesign:')
+        os.system('rclone copy /home/pi/New seniordesign:')
         os.system('rclone copy /home/pi/Documents/Measurement_Computing/Scanning_log_files seniordesign:')
         sleep(60)
         
